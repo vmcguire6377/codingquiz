2750edd79fb397d89a9da96ca030484696b8f7f0
VeeMcGee <vmcguire6377@outlook.com>
2021-04-19 17:46:43 -0600

    making final functionality adjustments

--- assets/js/mainel.js
+++ assets/js/mainel.js
@@ -1,69 +1,170 @@
-var mainEl = [
+// variables to keep track of quiz state
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
 
-    {
-        question: "Commonly used data types do NOT include:",
-        choices: "strings, booleans, alerts, numbers", 
-        answer: "alerts",
-    },
+// variables to reference DOM elements
+var questionsEl = document.getElementById("questions");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
 
-    {
-        question: "The condition in an if/else statement is enclosed within ______.",
-        choices: "quotes, parentheses, curly brackets, square brackets",
-        answer: "parentheses",
-    },
-    {
-        question: "The instructor, TAs, and tutors for this course are ______.",
-        choices: "phenomenally knowledgeable, readily available, positive and encouraging, all of the above",
-        answer: "all of the above",
-    },
-    {
-        question: "Arrays in JavaScript can be used to store ______",
-        choices: "numbers and strings, other arrays, booleans, all of the above",
-        answer: "all of the above",
-    },
-    {
+// sound effects
+var sfxRight = new Audio("assets/sfx/correct.wav");
+var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
-        question: "String values must be enclosed within ______ when being assigned to variables.",
-        choices: "quotes, commas, curly brackets, parentheses",
-        answer: "quotes",
-    }
-]
+function startQuiz() {
+	// hide start screen
+	var startScreenEl = document.getElementById("start-screen");
+	startScreenEl.setAttribute("class", "hide");
 
-var answersEl = [
-    {
-        choices: "strings, booleans, alerts, numbers",
-    },
-    {
-        choices: "quotes, parentheses, curly brackets, square brackets",
-    },
-    {
-        choices: "phenomenally knowledgeable, readily available, positive and encouraging, all of the above",
-    },
-    {
-        choices: "numbers and strings, other arrays, booleans, all of the above",
-    },
-    {
-        choices: "quotes, commas, curly brackets, parentheses",
-    }
+	// un-hide questions section
+	questionsEl.removeAttribute("class");
 
-    
-]
+	// start timer
+	timerId = setInterval(clockTick, 1000);
 
+	// show starting time
+	timerEl.textContent = time;
 
+	getQuestion();
+}
 
+function getQuestion() {
+	// get current question object from array
+	var currentQuestion = questions[currentQuestionIndex];
 
+	// update title with current question
+	var titleEl = document.getElementById("question-title");
+	titleEl.textContent = currentQuestion.title;
 
-console.log(answersEl);
-console.log(mainEl);
-console.log(mainEl[0].question);
-console.log(mainEl[1].answer);
-console.log(mainEl[1].choices);
-var test = [
-    "abc123", "678ulj", "bndk58694"
+	// clear out any old question choices
+	choicesEl.innerHTML = "";
 
+	// loop over choices
+	currentQuestion.choices.forEach(function (choice, i) {
+		// create new button for each choice
+		var choiceNode = document.createElement("button");
+		choiceNode.setAttribute("class", "choice");
+		choiceNode.setAttribute("value", choice);
 
-]
-console.log(test[1]);
+		choiceNode.textContent = i + 1 + ". " + choice;
 
-// make a conditional if /else to say if answer = correct alert "Correct!" else alert = "incorrect" which js file should the if statement go in?
-// then prompt next question
+		// attach click event listener to each choice
+		choiceNode.onclick = questionClick;
+
+		// display on the page
+		choicesEl.appendChild(choiceNode);
+	});
+}
+
+function questionClick() {
+	// check if user guessed wrong
+	if (this.value !== questions[currentQuestionIndex].answer) {
+		// penalize time
+		time -= 15;
+
+		if (time < 0) {
+			time = 0;
+		}
+
+		// display new time on page
+		timerEl.textContent = time;
+
+		// play "wrong" sound effect
+		sfxWrong.play();
+
+		feedbackEl.textContent = "Wrong!";
+	} else {
+		// play "right" sound effect
+		sfxRight.play();
+
+		feedbackEl.textContent = "Correct!";
+	}
+
+	// flash right/wrong feedback on page for half a second
+	feedbackEl.setAttribute("class", "feedback");
+	setTimeout(function () {
+		feedbackEl.setAttribute("class", "feedback hide");
+	}, 1000);
+
+	// move to next question
+	currentQuestionIndex++;
+
+	// check if we've run out of questions
+	if (currentQuestionIndex === questions.length) {
+		quizEnd();
+	} else {
+		getQuestion();
+	}
+}
+
+function quizEnd() {
+	// stop timer
+	clearInterval(timerId);
+
+	// show end screen
+	var endScreenEl = document.getElementById("end-screen");
+	endScreenEl.removeAttribute("class");
+
+	// show final score
+	var finalScoreEl = document.getElementById("final-score");
+	finalScoreEl.textContent = time;
+
+	// hide questions section
+	questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+	// update time
+	time--;
+	timerEl.textContent = time;
+
+	// check if user ran out of time
+	if (time <= 0) {
+		quizEnd();
+	}
+}
+
+function saveHighscore() {
+	// get value of input box
+	var initials = initialsEl.value.trim();
+
+	// make sure value wasn't empty
+	if (initials !== "") {
+		// get saved scores from localstorage, or if not any, set to empty array
+		var highscores =
+			JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+		// format new score object for current user
+		var newScore = {
+			score: time,
+			initials: initials
+		};
+
+		// save to localstorage
+		highscores.push(newScore);
+		window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+		// redirect to next page
+		window.location.href = "highscores.html";
+	}
+}
+
+function checkForEnter(event) {
+	// "13" represents the enter key
+	if (event.key === "Enter") {
+		saveHighscore();
+	}
+}
+
+// user clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+
+// user clicks button to start quiz
+startBtn.onclick = startQuiz;
+
+initialsEl.onkeyup = checkForEnter;
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,161 +1,36 @@
-
-var timeLeft = 75
-var timerEl = document.getElementById("myTimer");
-var startQuiz = document.getElementById("start");
-var sec = timeLeft;
-var timeDisplay = document.getElementById("myTimer")
-
-
-var question = document.getElementById("question");
-var image = document.getElementById("qImage");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD = document.getElementById("D");
-var correct = document.getElementById("correct");
-var incorrect = document.getElementById("incorrect");
-var choice = document.getElementsByName('choice');
-
-let questions = [{
-  question: "What is the name of the best selling rock band of all time?",
-  image: "./assets/images/music-notes.png",
-  choiceA: "Led Zeppelin",
-  choiceB: "The Eagles",
-  choiceC: "Aerosmith",
-  choiceD: "AC/DC",
-  correct: "C"
-},
-{
-  question: "What is the best selling pop album of all time?",
-  image: "./assets/images/grammy.jpg",
-  choiceA: "AC/DC, Back in Black",
-  choiceB: "Michael Jackson, Thriller",
-  choiceC: "MeatLoaf, Bat Out of Hell",
-  choiceD: "INXS, Kick",
-  correct: "B"
-},
-{
-  question: "Who is known as the 'Father of Rock'?",
-  image: "./assets/images/therock.jpg",
-  choiceA: "Elvis Presley",
-  choiceB: "Kenny G.",
-  choiceC: "Fats Domino",
-  choiceD: "Chuck Berry",
-  correct: "D"
-},
-{
-  question: "Who was the top female pop artist of the 1980s? ",
-  image: "./assets/images/pop.jpg",
-  choiceA: "Cher",
-  choiceB: "Madonna",
-  choiceC: "Whitney Houston",
-  choiceD: "Beyonce",
-  correct: "B"
-},
-{
-  question: "Would you like to try again?",
-  choiceA: "yes",
-  choiceB: "no",
-  choiceC: "unknown",
-  choiceD: "all of the above",
-  correct: ""
-}
+// list of all questions, choices, and answers
+var questions = [ 
+  
+   
+    {
+	  title: " What is the name of the best selling rock band of all time?", 
+		choices: ["Led Zeppelin", "The Eagles","Aerosmith", "AC/DC",
+		],
+		answer: "Aerosmith"
+	},
+  
+	{
+		title: "What is the best selling pop album of all time?",
+		choices: ["AC/DC, Back in Black","Michael Jackson, Thriller", "MeatLoaf, Bat Out of Hell", "INXS, Kick",
+		],
+		answer: "Michael Jackson, Thriller"
+	},
+	{
+		title: "Who is known as the 'Father of Rock'?",
+		choices: ["Elvis Presley","Kenny G.", "Fats Domino", "Chuck Berry",
+	],
+		answer: "Chuck Berry"
+	},
+	{
+		title: "Who was the top female pop artist of the 1980s?",
+		choices: ["Cher", "Madonna", "Whitney Houston", "Beyonce",
+	],
+		answer: "Madonna"
+	},
+	{
+		title:
+			"A very useful tool used during development and debugging for printing content to the debugger is:",
+		choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+		answer: "console.log"
+	}
 ];
-
-function myTimer() {
-  console.log(timeDisplay);
-  timerEl.innerHTML = timeLeft + "sec left";
-  sec--;
-}
-
-function endQuiz() {
-  if (runningquestion.questions === 0 || timeLeft === 0)
-    window.prompt("The game is over. Please enter your name.")
-}
-
-var lastquestion = questions.length - 1;
-let runningquestion = 0;
-let q;
-let cor = 0, inc = 0;
-
-startQuiz.addEventListener("click", function () {
-  countdown();
-}
-)
-
-function countdown() {
-  var timeInterval = setInterval(function () {
-    console.log("timeLeft", timeLeft);
-    if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + "seconds left";
-      timeLeft--;
-    }
-    else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + "second left";
-      timeLeft--;
-    }
-    else {
-      timerEl.textContent = "";
-      clearInterval(timeInterval);
-    }
-  }, 1000);
-
-}
-
-function askQuestion() {
-  q = questions[runningquestion];
-  question.innerHTML = "<p>" + q.question + "</p>";
-  image.innerHTML = "<img src = " + q.image + " width = '200px' height = '200px' >";
-  choiceA.innerHTML = "<input type='radio' value = 'A' name ='choice'/>" + q.choiceA;
-  choiceB.innerHTML = "<input type='radio' value = 'B' name ='choice'/>" + q.choiceB;
-  choiceC.innerHTML = "<input type='radio' value = 'C' name ='choice'/>" + q.choiceC;
-  choiceD.innerHTML = "<input type='radio' value = 'D' name ='choice'/>" + q.choiceD;
-  score();
-}
-
-function score() {
-  correct.innerHTML = "Correct : " + cor;
-  incorrect.innerHTML = "Incorrect : " + inc;
-}
-
-function next() {
-  for (var i = 0; i < choice.length; i++) {
-    if (choice[i].checked) {
-      if (choice[i].value === q.correct) {
-        alert("You Are Correct!");
-        cor++;
-      }
-      else {
-        alert("You are incorrect.");
-        console.log("inc", inc);
-        inc++;
-        timeLeft = timeLeft - 10;
-        console.log(timeLeft);
-      }
-    }
-
-  }
-
-  function endQuiz () {
-    if(timerEl.textContent = "") {
-      window.alert("The quiz is over")
-    }
-  }
-
-  runningquestion++;
-  askQuestion();
-  endQuiz();
-}
-
-localStorage.setItem("score", JSON.stringify(score));
-
-
-
-
-
-
-
-
-
-
-
